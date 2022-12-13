1b549ba1b989f65e3aaba838557f34dcaf86065c
Pakize Bozkurt <pakiozihak@gmail.com>
2022-12-13 09:18:00 +0000

    Get, Post, Put, Delete

--- hello.js
+++ hello.js
@@ -9,3 +9,22 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+
+//Path examples Get, Post, Put, Delete...
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/", (req, res) => {
+  res.send("Got a POST request");
+});
+
+app.put("/user", (req, res) => {
+  res.send("Got a PUT request at /user");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Got a DELETE request at /user");
+});
