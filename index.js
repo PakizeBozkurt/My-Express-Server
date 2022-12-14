bf4f0f4d33fb9a76a886b990c614c2dde6ed9f4a
Pakize Bozkurt <pakiozihak@gmail.com>
2022-12-14 10:18:20 +0000

    Completed it

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
-const express = require('express');
+const express = require("express");
 
+const exphbs = require('express-handlebars');
+const logger = require("./middleware/logger");
+const members = require('./members');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello there!!!</h1>')
-})
+//Init middleware
+//app.use(logger);
+
+//Handlebars Middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+//Homepage Route
+app.get('/', (req, res) => res.render('index', {
+    title: 'Member App',
+    members
+}));
+
+//Members API Routes
+app.use('api/members', require('./routes/api/members'))
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello there!!!</h1>");
+});
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
---
+++ members.js
@@ -0,0 +1,22 @@
+const members = [
+  {
+    id: 1,
+    name: "Pare",
+    email: "pare@gmail.com",
+    status: "active",
+  },
+  {
+    id: 2,
+    name: "Bare",
+    email: "bare@gmail.com",
+    status: "inactive",
+  },
+  {
+    id: 3,
+    name: "Ozzy",
+    email: "ozzy@gmail.com",
+    status: "active",
+  },
+];
+
+module.exports = members;
